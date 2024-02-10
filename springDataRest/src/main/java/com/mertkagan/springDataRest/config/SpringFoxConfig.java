4abb221c01f71cc79c567ca4ee059b0440bb3cf9
aqueelansari <aqueelansari1@gmail.com>
2024-02-10 22:13:09 +0530

    updated swagger config

--- springDataRest/src/main/java/com/mertkagan/springDataRest/config/SpringFoxConfig.java
+++ springDataRest/src/main/java/com/mertkagan/springDataRest/config/SpringFoxConfig.java
@@ -6,7 +6,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.BuilderDefaults.defaultIfAbsent;
@@ -21,6 +21,24 @@ public class SpringFoxConfig {
                 .paths(PathSelectors.any()).build();
     }
 
-
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
 
 }
